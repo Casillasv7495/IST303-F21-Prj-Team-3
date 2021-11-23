fa99429e7413b430bdbb129264beade0c6d0c9b0
Casillasv7495 <56240838+Casillasv7495@users.noreply.github.com>
2021-11-23 10:32:40 -0800

    Documents
    deleted documents I didn't need

--- ReservationsStatement.py
+++
@@ -1,7 +0,0 @@
-a=input("enter the items and costs ( diffrent item to be seperated by ',' ")
-
-print("****** RECiEPT *****")
-a=a.split(",")
-for i in a:
-    print(i)
-print("******* Thank you *****")
