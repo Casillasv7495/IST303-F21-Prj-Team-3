8fdfe9d4ba93af921afb72a73ce00986e5ed6564
Casillasv7495 <56240838+Casillasv7495@users.noreply.github.com>
2021-12-01 22:03:38 -0800

    fixed services
    fixed services

--- MiYE/Services/models.py
+++ MiYE/Services/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # ALWAYS run "python manage.py makemigrations" and "python manage.py migrate"
 # Create your models here.
 
-from django.db import models
 
 # Create your models here.
 class Types(models.Model):
@@ -35,4 +34,5 @@ class Services(models.Model):
     duration = models.IntegerField(choices=DURATION_CHOICES, default=30)
     Description = models.TextField(blank=True, null=True) # blank = field req; null => db
     Price = models.DecimalField(decimal_places=2, max_digits=10)
-   
+ 
+
